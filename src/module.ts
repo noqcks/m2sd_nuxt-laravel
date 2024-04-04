f46331f6a22e6792411afb68a24b67ce171b06a0
noqcks <benji@093b.org>
2024-04-04 19:36:40 +0000

    Upgrade dependencies

--- src/module.ts
+++ src/module.ts
@@ -222,7 +222,7 @@ const laravelModule: Module<Options> = function (overwrites) {
         logger.debug(`Laravel url: ${laravelUrl.href}`)
 
         try {
-          const server = execa(
+          const server = execa.command(
             `php artisan serve --host=${
               laravelUrl.hostname === 'localhost' ? '127.0.0.1' : laravelUrl.hostname
             } --port=${laravelUrl.port}`, 
